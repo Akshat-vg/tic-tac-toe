e3ec4589a15fef15911972659fb87df845437285
Akshat Ghanathay <93379911+Akshat-vg@users.noreply.github.com>
2023-11-03 23:00:25 +0530

    Revert "login page"

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Navbar, Nav } from 'react-bootstrap';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import Welcome from './Welcome';
-import './App.css';
-import Board from './Board';
-import Navbar from './Navbar';
+import React from "react";
 
-const PrivateRoute = ({ children, authed }) => {
-  return authed ? children : <Navigate to={"/"} />;
-};
+import './App.css';
+
+import Board from './Board'
+import Head from "./Head";
+
 
 function App() {
-  const [authed, setAuthed] = useState(false);
-  const googleClientId = `${process.env.REACT_APP_GOOGLE_CLIENT_ID}`;
   return (
-    <GoogleOAuthProvider clientId={googleClientId}>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Welcome setAuthed={setAuthed} />} />
-          <Route path="/board" element={<PrivateRoute authed={authed}><Board /></PrivateRoute>} />
-        </Routes>
-      </Router>
-    </GoogleOAuthProvider>
-  );
+    <div class="main-container">
+      <Head />
+      <Board />
+    </div>
+ );
 }
 
 export default App;
